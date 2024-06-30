ef27ca8e557f9fcc424b24b39a0bab39db193c18
BlazingBane <kondashivaradhan007@gmail.com>
2024-06-30 15:11:26 -0400

    diceDone

--- popup.tsx
+++ popup.tsx
@@ -1,39 +1,33 @@
 import { useEffect, useState } from "react"
+import { Storage } from "@plasmohq/storage"
+ 
+const storage = new Storage()
+
 import axios from 'axios';
 
 function IndexPopup() {
   const [data, setData] = useState("")
-  const [jsonData, setJsonData] = useState<any>(null);
-  async function sendData() {
-    setData("Code is getting executed")
-    try {
-      const response = await axios.post('http://127.0.0.1:5000/add_msg', {
-        message: "This is from extension"
-      });
-      setData("THis is inside the AXIOS")
-      console.log('Response:', response.data);
-      // Handle successful response here
-    } catch (error) {
-      console.error('Error:', error);
-      // Handle error here
-    }
-  }
- 
- 
+  const [email, setEmail] = useState("")
+  const [body, setBody] = useState("")
   return (
     <div
       style={{
         padding: 16
       }}>
-      <h2>
-        There is nothing to look at here
-      </h2>
-      <h3>
-        {data}
-      </h3>
-      <button onClick={()=>sendData()}>
-Click
-      </button>
+     <input type="email" placeholder="Enter your email" onChange={(e) => setData(e.target.value)} />
+     <textarea name="" onChange={(e) => setBody(e.target.value)} placeholder="Enter your message"></textarea>
+     <button onClick={() => {
+       
+       // Handle successful response here
+     }}>Submit</button>
+     <div>
+      <h3>active scritps</h3>
+      <ul>
+        <li>
+          dice Apply
+        </li>
+      </ul>
+     </div>
     </div>
   )
 }
